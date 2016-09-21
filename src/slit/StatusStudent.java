bcb85aea3c4c925f4d65a3d3ea1ad3949240f2fa
mariafuu <maria.fuglemsmo@gmail.com>
2016-09-21 11:44:17 +0200

    StatusStudent added

---
+++ src/slit/StatusStudent.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author maria
+ */
+package slit;
+
+import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import java.text.DecimalFormat;
+
+public class StatusStudent {
+    
+}
