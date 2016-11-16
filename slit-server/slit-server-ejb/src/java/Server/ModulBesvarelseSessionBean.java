b70f38f2a06c40718b4da5a9c2033460b6d8a432
bjornerik <bjorn.erik.vik.olsen@lyse.net>
2016-11-16 13:50:56 +0100

    ModulBesvarelseSessionBean

---
+++ slit-server/slit-server-ejb/src/java/Server/ModulBesvarelseSessionBean.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import DataModel.ModulBesvarelseDataModel;
+import Database.ModulbesvarelsePK;
+import Database.Modulbesvarelse;
+import Database.Bruker;
+import DataModel.BrukerDataModel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author bevo
+ */
+@Stateless
+public class ModulBesvarelseSessionBean implements ModulBesvarelseSessionBeanRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    @PersistenceContext(unitName = "slit-server-ejbPU")
+    private EntityManager em;
+    
+    public void persist(Object object) {
+        em.persist(object);
+    }
+    
+    //@Override
+    //public Date getModulBesvarelseTidspunktFromId(Date mb_id) {
+      //  return em.find(Modulbesvarelse.class, mb_id).getMbTidspunkt();
+                
+      
+    //}
+    
+    public ModulBesvarelseDataModel convertModulBesvarelse(Modulbesvarelse modulbesvarelse) 
+    {
+        ModulBesvarelseDataModel modulBesvarelseDataModel = new ModulBesvarelseDataModel(); 
+        
+        modulBesvarelseDataModel.setMbId(modulbesvarelse.getMbId());
+        modulBesvarelseDataModel.setMbTidspunkt(modulbesvarelse.getMbTidspunkt());
+        
+        
+        return modulBesvarelseDataModel; 
+    }
+    
+    public ModulBesvarelseDataModel convertModulBesvarelse(ModulbesvarelsePK modulbesvarelse) 
+    {
+        ModulBesvarelseDataModel modulBesvarelseDataModel = new ModulBesvarelseDataModel(); 
+        
+        modulBesvarelseDataModel.setMid(modulbesvarelse.getmid());
+        modulBesvarelseDataModel.setMid(modulbesvarelse.getmid());
+        
+        return modulBesvarelseDataModel; 
+    }
+    
+    /**
+     *
+     * @return ModulBesvarelseData
+     */
+    @Override
+    public List <ModulBesvarelseDataModel> getAllModulBesvarelse() {
+        
+        List<ModulBesvarelseDataModel> ModulBesvarelseData = new ArrayList <ModulBesvarelseDataModel> ();
+        
+        try
+        {
+            Query query = em.createNamedQuery("Modulbesvarelse.findAll", Modulbesvarelse.class);
+            
+            List<Modulbesvarelse> Modulbesvarelser = query.getResultList();
+            
+            for(Modulbesvarelse modulbesvarelse : Modulbesvarelser)
+            {
+                ModulBesvarelseData.add(this.convertModulBesvarelse(modulbesvarelse));
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return ModulBesvarelseData;
+    }
+
+    
+}
+
