8e20d4907883a3596c41417b24f2db601825fd1f
Adam Ajmi <adam.ajmi@gmail.com>
2016-12-08 15:02:05 +0100

    Pga. fiks på DB har jeg autogenerert en ny klasse av Tilbakemelding.java i Databasepakken. Entiteten refererer til modul for å vite hvilken modul den er en tilbakemelding på (istedenfor gjennom Modulbesvarelse vha JOIN) og inkluderer dato.

--- slit-server/slit-server-ejb/src/java/Database/Tilbakemelding.java
+++ slit-server/slit-server-ejb/src/java/Database/Tilbakemelding.java
@@ -6,41 +6,43 @@
 package Database;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Denne klassen sender spørringer mot databasen vår (MySQL) ved hjelp av
- * connectorJ som oversetter spørringen vi sender i Java/JDBC 
+ * connectorJ som oversetter spørringen vi sender i Java/JPQL 
  * (fra Payaraserveren) til MySQL. Det brukes også motsatt vei til å
- * oversette fra MySQL til JDBC slik at klassen kan motta informasjon
+ * oversette fra MySQL til JPQL slik at klassen kan motta informasjon
  * i respons av spørringene.
  * 
  * Mesteparten av klassen er autogenerert ved hjelp av ConnectorJ og Payara ved 
  * bruk av en connection pool til å lese tabellene i databasen vår.
- * @author bevo
+ * @author Adam
  */
 @Entity
-@Table(name = "Tilbakemelding")
+@Table(name = "tilbakemelding")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "Tilbakemelding.findAll", query = "SELECT t FROM Tilbakemelding t"),
-    @NamedQuery(name = "Tilbakemelding.findByTmID", query = "SELECT t FROM Tilbakemelding t WHERE t.tmID = :tmID"),
-    @NamedQuery(name = "Tilbakemelding.findByTmGodkjentstatus", query = "SELECT t FROM Tilbakemelding t WHERE t.tmGodkjentstatus = :tmGodkjentstatus"),
-    @NamedQuery(name = "Tilbakemelding.findByTmStudentkommentar", query = "SELECT t FROM Tilbakemelding t WHERE t.tmStudentkommentar = :tmStudentkommentar"),
-    @NamedQuery(name = "Tilbakemelding.findByTmLarerkommentar", query = "SELECT t FROM Tilbakemelding t WHERE t.tmLarerkommentar = :tmLarerkommentar")})
+    @NamedQuery(name = "Tilbakemelding_1.findAll", query = "SELECT t FROM Tilbakemelding_1 t"),
+    @NamedQuery(name = "Tilbakemelding_1.findByTmID", query = "SELECT t FROM Tilbakemelding_1 t WHERE t.tmID = :tmID"),
+    @NamedQuery(name = "Tilbakemelding_1.findByTmGodkjentstatus", query = "SELECT t FROM Tilbakemelding_1 t WHERE t.tmGodkjentstatus = :tmGodkjentstatus"),
+    @NamedQuery(name = "Tilbakemelding_1.findByTmDato", query = "SELECT t FROM Tilbakemelding_1 t WHERE t.tmDato = :tmDato"),
+    @NamedQuery(name = "Tilbakemelding_1.findByTmStudentkommentar", query = "SELECT t FROM Tilbakemelding_1 t WHERE t.tmStudentkommentar = :tmStudentkommentar"),
+    @NamedQuery(name = "Tilbakemelding_1.findByTmLarerkommentar", query = "SELECT t FROM Tilbakemelding_1 t WHERE t.tmLarerkommentar = :tmLarerkommentar")})
 public class Tilbakemelding implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -52,19 +54,16 @@ public class Tilbakemelding implements Serializable {
     @Basic(optional = false)
     @NotNull
     @Column(name = "tm_godkjentstatus")
-    private int tmGodkjentstatus;
+    private short tmGodkjentstatus;
+    @Column(name = "tm_dato")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date tmDato;
     @Size(max = 1000)
     @Column(name = "tm_studentkommentar")
     private String tmStudentkommentar;
     @Size(max = 1000)
     @Column(name = "tm_larerkommentar")
     private String tmLarerkommentar;
-    @JoinColumn(name = "mb_ID", referencedColumnName = "mb_id")
-    @ManyToOne(optional = false)
-    private Modulbesvarelse mbID;
-    @JoinColumn(name = "b_ID", referencedColumnName = "b_id")
-    @ManyToOne(optional = false)
-    private Bruker bid;
 
     public Tilbakemelding() {
     }
@@ -73,7 +72,7 @@ public class Tilbakemelding implements Serializable {
         this.tmID = tmID;
     }
 
-    public Tilbakemelding(Integer tmID, int tmGodkjentstatus) {
+    public Tilbakemelding(Integer tmID, short tmGodkjentstatus) {
         this.tmID = tmID;
         this.tmGodkjentstatus = tmGodkjentstatus;
     }
@@ -86,12 +85,20 @@ public class Tilbakemelding implements Serializable {
         this.tmID = tmID;
     }
 
-    public int getTmGodkjentstatus() {
+    public short getTmGodkjentstatus() {
         return tmGodkjentstatus;
     }
 
-    public void setTmGodkjentstatus(int tmGodkjentstatus) {
+    public void setTmGodkjentstatus(short tmGodkjentstatus) {
         this.tmGodkjentstatus = tmGodkjentstatus;
+    }
+
+    public Date getTmDato() {
+        return tmDato;
+    }
+
+    public void setTmDato(Date tmDato) {
+        this.tmDato = tmDato;
     }
 
     public String getTmStudentkommentar() {
@@ -108,22 +115,6 @@ public class Tilbakemelding implements Serializable {
 
     public void setTmLarerkommentar(String tmLarerkommentar) {
         this.tmLarerkommentar = tmLarerkommentar;
-    }
-
-    public Modulbesvarelse getMbID() {
-        return mbID;
-    }
-
-    public void setMbID(Modulbesvarelse mbID) {
-        this.mbID = mbID;
-    }
-
-    public Bruker getBid() {
-        return bid;
-    }
-
-    public void setBid(Bruker bid) {
-        this.bid = bid;
     }
 
     @Override
@@ -148,7 +139,7 @@ public class Tilbakemelding implements Serializable {
 
     @Override
     public String toString() {
-        return "Database.Tilbakemelding[ tmID=" + tmID + " ]";
+        return "Database.Tilbakemelding_1[ tmID=" + tmID + " ]";
     }
     
 }
