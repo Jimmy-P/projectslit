5961b4cfb027788cdbbeeb18e3933eb681664861
mariafuu <maria.fuglemsmo@gmail.com>
2016-12-13 09:14:52 +0100

    Flere forsøk på oversikt

--- slit-client/src/java/Framework/TeacherOversiktManager.java
+++ slit-client/src/java/Framework/TeacherOversiktManager.java
@@ -5,18 +5,98 @@
  */
 package Framework;
 
-import java.util.*;  
+import DataModel.BrukerDataModel;
+import java.util.*;
+import GUI.TeacherMenuStudentoversiktController;
+import DataModel.BrukerDataModel;
+import Framework.BrukerManager;
+import java.net.URL;
+import java.util.*;
+import javafx.fxml.FXML;
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.beans.property.SimpleIntegerProperty;
+import Framework.OversiktEnhet;
 
 /**
  *
  * @author Maria
  */
 public class TeacherOversiktManager {
-     /*
-    ArrayList<String> oversikt = new ArrayList();
+     
     
-    for (int i =0; i < list.size(); i++) {
-        
-    }*/
+    private int brukerID;
+    private String brukerfornavn;
+    private String brukeretternavn;
+    private String brukerepost;
+    private int brukertype;
+    
+    /*
+    public void newArrayList() {
+    List<OversiktEnhet> oversiktEnheterList = new ArrayList();
+    
+    OversiktEnhet oe1 = new OversiktEnhet();
+    OversiktEnhet.setFNavn("Will");
+    OversiktEnhet oe2 = new OversiktEnhet("Angelina", "Jolie", "ajolie@online.no", 3, 3, 2, 1, 1, 1, 1, 1, 1, 1);
+    OversiktEnhet oe3 = new OversiktEnhet("Jamie", "Bell", "jbell@gmail.com", 3, 3, 3, 3, 3, 3, 3, 2, 1, 1);
+    OversiktEnhet oe4 = new OversiktEnhet("Meryl", "Streep", "mstreep@gmail.com", 2, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+    OversiktEnhet oe5 = new OversiktEnhet("Lena", "Heady", "lheady@online.no", 3, 3, 2, 2, 1, 1, 1, 1, 1, 1);
+    
+}
+    }
+    
+    newOversiktEnhet("Will", "Smith", "wsmith@online.no", 3, 3, 3, 2, 2, 1, 1, 1, 1, 1);
+    
+    //ArrayList<String> oversikt = new ArrayList();
+    //int i = 0;
+    
+    //for (i = 0; i < 10; i++) {
+    //    testEvent() throws Exception;
+    //}
+    
+    /*
+    @FXML
+    public void lagStudentListe() {
+        ArrayList<Student> studentListe = new ArrayList();
+        int i = 0;
+        BrukerManager bm = new BrukerManager();
+        for (BrukerDataModel bdm : bm.getAllBrukere()) {
+            brukerID = BrukerManager.getbID();
+            brukerfornavn = BrukerManager.getBrukerFNavnFromId(i);
+            brukeretternavn = BrukerManager.getbEnavn();
+            brukerepost = BrukerManager.getbEmail();
+            brukertype = BrukerManager.getbType();
+            
+            if (brukertype == 1) {
+                Student student = new Student();
+                this.student(setStudentID(brukerID));
+                setStudentFName(brukerfornavn);
+                setStudentEName(brukeretternavn);
+                setStudentMail(brukerepost);
+                
+                studentListe.add(student);
+            }
+            i++;
+        }
+        //TeacherMenuStudentoversiktController.setTextArea(brukerListe.toString() + "\n");
+    }
+    */
+    
+    
+    @FXML
+    private void testEvent() throws Exception {
+        BrukerManager bm = new BrukerManager();
+        for (BrukerDataModel bdm : bm.getAllBrukere()) {
+            System.out.println("");
+            System.out.println(bdm.getbFnavn() + " " + bdm.getbEnavn());
+            System.out.println(bdm.getbEmail());
+        }
+    }
+    
     
 }
--- slit-client/src/java/GUI/TeacherMenuStudentoversiktController.java
+++ slit-client/src/java/GUI/TeacherMenuStudentoversiktController.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import DataModel.BrukerDataModel;
 import Names.ViewNames;
 import javafx.scene.control.*;
 import DataModel.OversiktDataModel;
@@ -10,8 +11,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.control.cell.*;
+import Framework.TeacherOversiktManager;
+import Framework.OversiktEnhet;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -59,14 +61,68 @@ public class TeacherMenuStudentoversiktController implements Initializable {
         }
     
     @FXML
-    private TableView<OversiktDataModel> oversiktTableView;
-
+    private TextArea testTextArea;
+    
     @FXML
+    private TableView<OversiktDataModel> oversiktTableView;
+    
     private TableColumn<OversiktDataModel, String> fNameColumn;
-
+    private TableColumn<OversiktDataModel, String> eNameColumn;
+    private TableColumn<OversiktDataModel, String> mailColumn;
+    private TableColumn<OversiktDataModel, String> mod1Column;
+    private TableColumn<OversiktDataModel, String> mod2Column;
+    private TableColumn<OversiktDataModel, String> mod3Column;
+    private TableColumn<OversiktDataModel, String> mod4Column;
+    private TableColumn<OversiktDataModel, String> mod5Column;
+    private TableColumn<OversiktDataModel, String> mod6Column;
+    private TableColumn<OversiktDataModel, String> mod7Column;
+    private TableColumn<OversiktDataModel, String> mod8Column;
+    private TableColumn<OversiktDataModel, String> mod9Column;
+    private TableColumn<OversiktDataModel, String> mod10Column;
+    
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-    fNameColumn.setCellValueFactory(new PropertyValueFactory<OversiktDataModel, String>("fNameColumn"));
+    public void initialize (URL location, ResourceBundle resources) {
+    //    testTextArea.textProperty().bind();
+    }
+    
+    public void fillOversiktTableView(String text) {
+        fNameColumn.setCellValueFactory(
+                new PropertyValueFactory<>("firstName"));
+        eNameColumn.setCellValueFactory(
+                new PropertyValueFactory<>("lastName"));
+        mailColumn.setCellValueFactory(
+                new PropertyValueFactory<>("email"));
+        mod1Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod1"));
+        mod2Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod2"));
+        mod3Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod3"));
+        mod4Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod4"));
+        mod5Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod5"));
+        mod6Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod6"));
+        mod7Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod7"));
+        mod8Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod8"));
+        mod9Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod9"));
+        mod10Column.setCellValueFactory(
+                new PropertyValueFactory<>("mod10"));
+ 
+        //oversiktTableView.setItems(OversiktEnhet.oversiktEnheter);
+        oversiktTableView.getColumns().addAll(fNameColumn, eNameColumn, mailColumn,
+                mod1Column, mod2Column, mod3Column, mod4Column, mod5Column,
+                mod6Column, mod7Column, mod8Column, mod9Column, mod10Column);
+    }
+
+
+    //@Override
+    //public void initialize(URL location, ResourceBundle resources) {
+    //fNameColumn.setCellValueFactory(new PropertyValueFactory<OversiktDataModel, String>("fNameColumn"));
 
     //oversiktTableView.getItems().setAll(getbFnavn());
 }
@@ -74,4 +130,14 @@ public class TeacherMenuStudentoversiktController implements Initializable {
 //private List<OversiktDataModel> getbFnavn(){
 // this method would fetch the necessary items from database.
 //}
-}
+    /*
+    @FXML
+    private void testEvent() throws Exception {
+        BrukerManager bm = new BrukerManager();
+        for (BrukerDataModel bdm : bm.getAllBrukere()) {
+            System.out.println("");
+            System.out.println(bdm.getbFnavn() + " " + bdm.getbEnavn());
+            System.out.println(bdm.getbEmail());
+        }
+    }*/
+
